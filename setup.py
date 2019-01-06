f2d2606eaee026d51435a6c90942a56b77bd1134
Anselm Joseph <an23lm@Anselms-MacBook-Air.local>
2019-01-07 01:22:02 +0530

    rename project to git bundle plus

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='Git Archive',  
+     name='Git Bundle Plus',  
      version='0.9',
      author="Ansèlm Joseph",
      author_email="anselmjosephs@gmail.com",
      description="Bundle a local Git project with all uncommitted changes and stashes (git bundle on steroids).",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/an23lm/GitArchive",
-     scripts=['gitarchive'],
+     url="https://github.com/an23lm/GitBundlePlus",
+     scripts=['gitbundleplus'],
      packages=['modules'],
      classifiers=[
          "Programming Language :: Python :: 3",
